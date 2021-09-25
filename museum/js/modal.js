2da7e2ff385fe98cf114704ffb13f9ade735cff2
Denis Popov <popovdw94@gmail.com>
2021-09-26 02:05:25 +0300

    fix: wrap code with function

--- museum/js/modal.js
+++ museum/js/modal.js
@@ -1,27 +1,30 @@
-const modal = document.getElementById("booking");
-const overlay = document.getElementsByClassName("modal-overlay")[0];
-const btn = document.getElementById("buyNow");
-const span = document.getElementsByClassName("modal-close")[0];
-const ANIMATION_SPEED = 500;
-let closing = true;
+function modalActivity() {
+  const modal = document.getElementById("booking");
+  const overlay = document.getElementsByClassName("modal-overlay")[0];
+  const btn = document.getElementById("buyNow");
+  const span = document.getElementsByClassName("modal-close")[0];
+  const ANIMATION_SPEED = 500;
+  let closing = true;
 
-btn.onclick = function () {
-  modal.classList.add("open");
-};
+  btn.onclick = function () {
+    modal.classList.add("open");
+  };
 
-span.onclick = function () {
-  modal.classList.remove("open");
-  closing = false;
-};
-
-window.onclick = function (event) {
-  if (event.target == overlay) {
-    closing = true;
+  span.onclick = function () {
     modal.classList.remove("open");
-    modal.classList.add("hide");
-    setTimeout(() => {
-      modal.classList.remove("hide");
-      closing = false;
-    }, ANIMATION_SPEED);
-  }
-};
+    closing = false;
+  };
+
+  window.onclick = function (event) {
+    if (event.target == overlay) {
+      closing = true;
+      modal.classList.remove("open");
+      modal.classList.add("hide");
+      setTimeout(() => {
+        modal.classList.remove("hide");
+        closing = false;
+      }, ANIMATION_SPEED);
+    }
+  };
+}
+modalActivity();
