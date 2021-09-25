7848a5ff5fd2f237ffad2190988f77c48ae39ed7
Denis Popov <popovdw94@gmail.com>
2021-09-26 00:13:24 +0300

    feat: add booking form with styles, images and jscript

---
+++ museum/js/modal.js
@@ -0,0 +1,27 @@
+const modal = document.getElementById("booking");
+const overlay = document.getElementsByClassName("modal-overlay")[0];
+const btn = document.getElementById("buyNow");
+const span = document.getElementsByClassName("modal-close")[0];
+const ANIMATION_SPEED = 500;
+let closing = true;
+
+btn.onclick = function () {
+  modal.classList.add("open");
+};
+
+span.onclick = function () {
+  modal.classList.remove("open");
+  closing = false;
+};
+
+window.onclick = function (event) {
+  if (event.target == overlay) {
+    closing = true;
+    modal.classList.remove("open");
+    modal.classList.add("hide");
+    setTimeout(() => {
+      modal.classList.remove("hide");
+      closing = false;
+    }, ANIMATION_SPEED);
+  }
+};
