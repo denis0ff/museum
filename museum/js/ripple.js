5a109d90447c1da150046b3071b13ff53a6325c9
Denis Popov <popovdw94@gmail.com>
2021-09-27 19:51:06 +0300

    fix: minor fixes

---
+++ museum/js/ripple.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.ripple')
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft - 916
+        const yInside = y - buttonTop - 192
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
