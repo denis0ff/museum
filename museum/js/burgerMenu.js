a2b98c501870bf267eb18fbb0909471e4a3fe693
Denis Popov <popovdw94@gmail.com>
2021-09-29 23:40:50 +0300

    feat: add adaptive for header, welcome, visiting and explore blocks

---
+++ museum/js/burgerMenu.js
@@ -0,0 +1,23 @@
+function burgerMenu() {
+  let menu = document.querySelector("nav.header-nav");
+  let button = document.querySelector("a.burger-menu_button");
+  let links = menu.querySelectorAll('li.header-list-item > a')
+  let trigger = button.querySelector('span.burger-menu_lines')
+  let title = document.querySelector("div.title");
+  button.addEventListener("click", toggle);
+  links.forEach(item => item.addEventListener("click", toggle));
+  function toggle() {
+    if (menu.classList.contains('active')) {
+      menu.classList.remove('active')
+      button.classList.remove("active");
+      trigger.classList.remove("active");
+      title.classList.remove("active");
+    } else {
+      menu.classList.add("active");
+      button.classList.add("active");
+      trigger.classList.add("active");
+      title.classList.add("active");
+    }
+  }
+}
+burgerMenu();
